fe4e6b62031c17b096e068bf1e4d261f3ad73e54
Nilesh Jadhav <30795435+nilesh-jadhav@users.noreply.github.com>
2021-08-11 19:46:57 +0530

    Add files via upload
    created base project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bake Sales For Good</h1>
+      </header>
+      <section>
+        content
+      </section>
+    </div>
+  );
+}
+
+export default App;
